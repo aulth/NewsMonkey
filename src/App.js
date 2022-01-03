ab38ccb6b625dbde7901f3768246bd60a79d2c8f
aulth <mohdusman.you@gmail.com>
2022-01-03 13:33:24 +0530

    Added infinite scroll

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ import About from './components/About';
 import Footer from './components/Footer';
 
 export default class App extends Component {
-  pageSize = 21;
+  pageSize = 12;
   render() {
     return (
       <div>
         <Router>
         <Navbar/>
           <Routes>
-              <Route path="/" element={<News key='general' country='in' category='general' pageSize={this.pageSize} />} />
+              <Route path="/" element={<News key='general' country='us' category='general' pageSize={this.pageSize} />} />
               <Route path="/about" element={<About/> } />
-              <Route path="/business" element={<News key='business'  country='in' category='business' pageSize={this.pageSize} />} />
-              <Route path="/entertainment" element={<News key='entertainment'  country='in' category='entertainment' pageSize={this.pageSize} />} />
-              <Route path="/general" element={<News key='general'  country='in' category='general' pageSize={this.pageSize} />} />
-              <Route path="/health" element={<News key='health'  country='in' category='health' pageSize={this.pageSize} />} />
-              <Route path="/science" element={<News key='science'  country='in' category='science' pageSize={this.pageSize} />} />
-              <Route path="/sports" element={<News key='sports'  country='in' category='sports' pageSize={this.pageSize} />} />
-              <Route path="/technology" element={<News key='technology'  country='in' category='technology' pageSize={this.pageSize} />} />
+              <Route path="/business" element={<News key='business'  country='us' category='business' pageSize={this.pageSize} />} />
+              <Route path="/entertainment" element={<News key='entertainment'  country='us' category='entertainment' pageSize={this.pageSize} />} />
+              <Route path="/general" element={<News key='general'  country='us' category='general' pageSize={this.pageSize} />} />
+              <Route path="/health" element={<News key='health'  country='us' category='health' pageSize={this.pageSize} />} />
+              <Route path="/science" element={<News key='science'  country='us' category='science' pageSize={this.pageSize} />} />
+              <Route path="/sports" element={<News key='sports'  country='us' category='sports' pageSize={this.pageSize} />} />
+              <Route path="/technology" element={<News key='technology'  country='us' category='technology' pageSize={this.pageSize} />} />
           </Routes>
           <Footer/>
         </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -2,78 +2,84 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
-  static defaultProps ={
-    country : 'us',
-    category : 'general',
-    apiKey : '6328182ecb4d4dd9b91de9eeec9bef8a',
-    pageSize : '21',
+  static defaultProps = {
+    // country : 'us',
+    category: 'general',
+    apiKey: '6328182ecb4d4dd9b91de9eeec9bef8a',
+    pageSize: '21',
   }
   static propTypes = {
-    country : PropTypes.string.isRequired,
-    category : PropTypes.string.isRequired,
-    apiKey : PropTypes.string.isRequired,
-    pageSize : PropTypes.number.isRequired,
+    // country : PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    apiKey: PropTypes.string.isRequired,
+    pageSize: PropTypes.number.isRequired,
   }
-    constructor(){
-      super();
-      this.state = {
-          articles : [],
-          loading : false,
-          page : 1,
-          totalResults: '',
-      }
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: '',
     }
-    async updateNews(){
-      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-      this.setState({loading:true});
-      let data = await fetch(url);
-      let parsedData =await data.json();
-      this.setState({
-        articles:parsedData.articles,
-        loading : false
-      });
-    }
-    async componentDidMount(){
-      this.updateNews();
-    }
+  }
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      loading: false,
+      totalResults: parsedData.totalResults,
+    });
+    console.log(parsedData)
+  }
 
-    handleNext = async ()=>{
-      this.setState({page : this.state.page+1})
-      this.updateNews();
-    }
-    handlePrev = async ()=>{
-      this.setState({page:this.state.page - 1});
-      this.updateNews();
-      }
-    render() {
-        return (
-            <div className='container my-3'>
-              {/* {!this.state.loading && <h2 className='text-center' >Todays Top-Headlines in {this.props.category.split('').map(item=>{item[0].toUpperCase()+item.slice(1).toLowerCase()).join('')}} </h2>} */}
-              {this.state.loading && <Spinner/>}
-                <div className="row"> 
-                    { !this.state.loading && this.state.articles.map((item)=>{
-                        return <div key={item.url} className="col-md-4">
-                                    <NewsItem 
-                                      title={item.title?item.title.slice(0,80) +'...':' '} 
-                                      description={item.description?item.description.slice(0,90)+'...':' '} 
-                                      newsUrl={item.url} imageUrl={item.urlToImage?(item.urlToImage.includes('https://i-invdn-com.investing.com/news/')?item.urlToImage.replace('https://i-invdn-com.investing.com/news/',''):item.urlToImage):'https://st.depositphotos.com/1006899/3776/i/950/depositphotos_37765339-stock-photo-news.jpg'} 
-                                      date={item.publishedAt.slice(0,10)} 
-                                      source = {item.source.name?item.source.name:''}
-                                    />
-                                </div>
-                    })}
-                </div>
-                <div className="container d-flex justify-content-between my-3">
-                <button type="button" disabled={this.state.page<=1} className="btn btn-warning" onClick={this.handlePrev} >&laquo;Prev</button>
-                <button type="button" style={{display:this.state.page<=1?'none':'inline'}} className="btn btn-warning" onClick={this.handlePrev} >Page : {this.state.page}</button>
-                <button type="button" disabled={this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-warning" onClick={this.handleNext} >Next &raquo;</button>
-                </div>
-            </div>
+  async componentDidMount() {
+    this.updateNews();
+  }
 
-        )
-    }
+  loadMoreData = async ()=>{
+    this.setState({ page: this.state.page + 1 })
+    this.updateNews();
+  }
+  
+  render() {
+    return (
+      <InfiniteScroll
+          dataLength={this.state.articles.length} 
+          next={this.loadMoreData}
+          hasMore={this.state.articles.length!==this.state.totalResults}
+          loader={<Spinner/>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+        <div className='container my-3'>
+        <div className="row">
+          {this.state.articles.map((item) => {
+            return <div key={item.url} className="col-md-4">
+                    <NewsItem
+                      title={item.title ? item.title.slice(0, 80) + '...' : ' '}
+                      description={item.description ? item.description.slice(0, 90) + '...' : ' '}
+                      newsUrl={item.url} imageUrl={item.urlToImage ? (item.urlToImage.includes('https://i-invdn-com.investing.com/news/') ? item.urlToImage.replace('https://i-invdn-com.investing.com/news/', '') : item.urlToImage) : 'https://st.depositphotos.com/1006899/3776/i/950/depositphotos_37765339-stock-photo-news.jpg'}
+                      date={item.publishedAt.slice(0, 10)}
+                      source={item.source.name ? item.source.name : ''}
+                    />
+                  </div>
+          })}
+        </div>
+      </div>
+        </InfiniteScroll>
+
+    );
+  }
 }
 
 export default News
